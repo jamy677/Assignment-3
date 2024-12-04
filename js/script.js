3e86b8486d14a8a826bc231b7f21d87a048e189e
Aryan Jamwal <137585537+jamy677@users.noreply.github.com>
2024-12-03 19:52:20 -0500

    adding api keys with elements in js.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const weatherApiKey = '8e20a393d1eabed85a9a85cf2722ea59';
+const unsplashApiKey = 'K1VC-ki0TULrqLYzj9f_hIeIfo120MBk6YJN5ibAufU'; 
+
+// Elements
+const cityInput = document.getElementById('cityInput');
+const weatherInfo = document.getElementById('weatherInfo');
+const cityImage = document.getElementById('cityImage');
+const cityForm = document.getElementById('cityForm');
